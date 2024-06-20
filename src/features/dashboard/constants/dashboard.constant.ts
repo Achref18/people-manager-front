985870f722f936706f415808789ce4923e37fb1c
Achref <saidiachref70@gmail.com>
2024-06-20 12:36:07 +0100

    Create dashboard constants

---
+++ src/features/dashboard/constants/dashboard.constant.ts
@@ -0,0 +1,13 @@
+export const drawerWidth = 240;
+
+export const dashboardConstants = {
+  DASHBOARD: "Dashboard",
+  SEARCH_PLACEHOLDER: "Search…",
+  MESSAGES: "Messages",
+  NOTIFICATIONS: "Notifications",
+  SETTINGS: "Settings",
+  ADMIN: "Admin",
+  LOGOUT: "Logout",
+  NOTIF_CONTENT: 17,
+  EMAIL_CONTENT: 4,
+};
