997531438af18c206103176470e02613a87b8f92
Achref <saidiachref70@gmail.com>
2024-06-20 12:43:58 +0100

    Create main constants

---
+++ src/features/main/main.constants.ts
@@ -0,0 +1,22 @@
+import { globalVariables } from "@config/globalConstants/globalVariables";
+
+export const mainConstants = {
+  USERS: "Users",
+  SEARCH_PLACEHOLDER: "Search for users",
+  ADD_USER: "Add user",
+  FIRST_NAME: "First Name",
+  LAST_NAME: "Last Name",
+  GENDER: "Gender",
+  MALE: "Male",
+  FEMALE: "Female",
+  CANCEL: "Cancel",
+  Add: "Add",
+};
+
+export const USER_TABLE_HEADER = [
+  "First Name",
+  "Last Name",
+  "Gender",
+  "Birth Date",
+  globalVariables.EmptyString,
+];
